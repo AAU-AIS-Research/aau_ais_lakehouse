51033e2af752abe2c6ada44d854b0e601ab09c0a
Kasper Fromm Pedersen <kasperf@cs.aau.dk>
2026-05-20 04:01:30 +0000

    Reduced ingest chunk size for traj and stop dimensions

--- packages/schema/src/aau_ais_schema/dim/traj_geom_dim.py
+++ packages/schema/src/aau_ais_schema/dim/traj_geom_dim.py
@@ -60,5 +60,5 @@ class TrajGeomDim(Dimension):
             columns=keys + attributes,
             merge_strategy=merge_strategy,
             pre_processors=pre_processors,
-            max_ingest_chunk_size=1000000,
+            max_ingest_chunk_size=500000,
         )
