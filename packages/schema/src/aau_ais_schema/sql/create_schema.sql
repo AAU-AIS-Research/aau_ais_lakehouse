e3f6f9703375aff544f12a2552176504495dee7c
Kasper Fromm Pedersen <kasperf@cs.aau.dk>
2026-08-14 10:45:42 +0000

    Added a new ts_to_temporal_part_key macro

--- packages/schema/src/aau_ais_schema/sql/create_schema.sql
+++ packages/schema/src/aau_ais_schema/sql/create_schema.sql
@@ -205,6 +205,10 @@ create or replace macro grid_id_to_wgs84_envelope(grid_id, cell_width, cell_heig
 create or replace macro datetime_keys_to_timestamp(date_id, time_id) as (
     select strptime(date_id::text || lpad(time_id::text, 6, '0'), '%Y%m%d%H%M%S')
 );
+
+create or replace macro ts_to_temporal_part_key(ts) as
+    year(ts) * 100 + month(ts);
+comment on macro ts_to_temporal_part_key is 'Converts a timestamp to a temporal partitioning key';
 --#endregion
 --------------------------------------------------------------------------------------
 --#region Dimensions
